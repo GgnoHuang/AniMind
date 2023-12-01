d79e4a6bec28d85ccc15e14483f9c864ac88df8f
GgnoHuang <lshapeddesk@icloud.com>
2023-12-01 21:52:20 +0800

    多人協作測試

--- src/components/ydoc.js
+++ src/components/ydoc.js
@@ -1,20 +1,22 @@
-// import * as Y from 'yjs'
+import * as Y from 'yjs'
 
-// import { WebsocketProvider } from 'y-websocket'
+import { WebsocketProvider } from 'y-websocket'
 
-// const ydoc = new Y.Doc()
+const ydoc = new Y.Doc()
 
-// // 【用websocket的注意了！】https下浏览器只允许使用wss协议,不允许使用ws协议！注意了！
-// // const wsProvider = new WebsocketProvider('ws://localhost:1234', 'my-roomname', ydoc)
+// 【用websocket的注意了！】https下浏览器只允许使用wss协议,不允许使用ws协议！注意了！
+// const wsProvider = new WebsocketProvider('ws://localhost:1234', 'my-roomname', ydoc)
 
-// const wsProvider = new WebsocketProvider('ws://localhost:1234', 'my', ydoc)
-// // const wsProvider = new WebsocketProvider(
-// //   'wss://demos.yjs.dev', 'quill-demo-room', ydoc
-// //   )// 這個可以連上
+const wsProvider = new WebsocketProvider('ws://localhost:1234', 'my', ydoc)
 
-// wsProvider.on('status', event => {
-//   console.log(event.status) // logs "connected" or "disconnected"
-// })
-// export default ydoc;
-// // 這個有connected
+// const wsProvider = new WebsocketProvider(
+//   'wss://demos.yjs.dev', 'quill-demo-room', ydoc
+//   )
+// 這個可以連上
+
+wsProvider.on('status', event => {
+  console.log(event.status) // logs "connected" or "disconnected"
+})
+export default ydoc;
+// 這個有connected
 
